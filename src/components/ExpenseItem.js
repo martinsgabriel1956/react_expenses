d7b43b719c6e0de563a952ecce59b1e3730ea3bf
Gabriel Martins <martinsgabriel1956@gmail.com>
2021-06-23 14:00:12 -0300

    feat: writing more complex JSX code

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,11 @@
 export function ExpenseItem() {
   return (
-    <h2>I'm a Expense Item!</h2>
+    <div>
+      <div>March 28th 2021</div>
+      <div>
+        <h2>Car Insurance</h2>
+        <div>$294.67</div>
+      </div>
+    </div>
   );
 }
