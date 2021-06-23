bc5448c99dc5a7e76527ce2d02e11e8cc6cab008
Gabriel Martins <martinsgabriel1956@gmail.com>
2021-06-23 17:20:14 -0300

    feat: working with event handlers

--- src/components/Expenses/ExpenseItem/index.js
+++ src/components/Expenses/ExpenseItem/index.js
@@ -4,6 +4,10 @@ import { Card } from '../../UI/Card';
 import './style.css'
 
 export function ExpenseItem(props) {
+  function handleClick() {
+    console.log("Clicked!!!")
+  }
+
   return (
     <Card className='expense-item'>
       <ExpenseDate date={props.date} />
@@ -11,6 +15,7 @@ export function ExpenseItem(props) {
         <h2>{props.title}</h2>
         <div className='expense-item__price'>${props.amount}</div>
       </div>
+      <button onClick={handleClick}>Change title</button>
     </Card>
   );
 }
