100576e77455fafcf36978f29c934462653fb9a1
Gabriel Martins <martinsgabriel1956@gmail.com>
2021-06-23 14:41:18 -0300

    feat: challenge resolved

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import React from 'react';
-import { ExpenseItem } from './components/ExpenseItem/';
+import { Expenses } from './components/Expenses';
 
 export function App() {
   const expenses = [
@@ -26,26 +25,7 @@ export function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem 
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      />
-      <ExpenseItem 
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      />
-      <ExpenseItem 
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-      />
-      <ExpenseItem 
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
-      />
+      <Expenses items={expenses}/>
     </div>
   );
 }
