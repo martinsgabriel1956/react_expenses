888312baabfec04da1b662e527aec1547d587b62
Gabriel Martins <martinsgabriel1956@gmail.com>
2021-06-23 17:56:20 -0300

    feat: working with state

--- src/components/Expenses/ExpenseItem/index.js
+++ src/components/Expenses/ExpenseItem/index.js
@@ -1,16 +1,28 @@
+import React, { useState } from 'react'
+
 import { ExpenseDate } from '../../Expenses/ExpenseDate';
 import { Card } from '../../UI/Card';
 
 import './style.css'
 
 export function ExpenseItem(props) {
+  const [title, setTitle] = useState(props.title);
+
+
+
+  function handleClick() {
+    setTitle("Updated!");
+    console.log("Clicked!!!");
+  }
+
   return (
     <Card className='expense-item'>
       <ExpenseDate date={props.date} />
       <div className='expense-item__description'>
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className='expense-item__price'>${props.amount}</div>
       </div>
+      <button onClick={handleClick}>Change title</button>
     </Card>
   );
 }
