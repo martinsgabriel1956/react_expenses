2975c2a051c60fd58ab51b2439f35332c35ed9ca
Gabriel Martins <martinsgabriel1956@gmail.com>
2021-06-24 11:10:39 -0300

    feat: working with multiple states

--- src/components/NewExpense/ExpenseForm/index.js
+++ src/components/NewExpense/ExpenseForm/index.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react'
+
 
 import './style.css';
 
 export function ExpenseForm() {
+  const [enteredTitle, setEnteredTitle] = useState('');
+  const [enteredAmount, setEnteredAmount] = useState(0);
+  const [enteredDate, setEnteredDate] = useState('');
+
   function handlerTitleChange(e) {
-    console.log(e.target.value)
+    setEnteredTitle(e.target.value)
   }
+  
+  function handlerAmountChange(e) {
+    setEnteredAmount(e.target.value);
+  }
+  
+  function handlerDateChange(e) {
+    setEnteredDate(e.target.value);
+  }
+
 
   return (
     <form>
@@ -16,11 +30,11 @@ export function ExpenseForm() {
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
-          <input type="number" min="0.01" step="0.01" />
+          <input type="number" min="0.01" step="0.01" onChange={handlerAmountChange} />
         </div>
         <div className="new-expense__control">
           <label>Date</label>
-          <input type="date" min="2019-01-01" max="2022-12-31" />
+          <input type="date" min="2019-01-01" max="2022-12-31" onChange={handlerDateChange} />
         </div>
       </div>
       <div className="new-expense__actions">
