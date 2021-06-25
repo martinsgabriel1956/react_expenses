04e8e07774a2a77eb60380f21ea41512721929b1
Gabriel Martins <martinsgabriel1956@gmail.com>
2021-06-25 11:17:35 -0300

    feat: adding dynamic styles

--- src/components/Chart/ChartBar/index.js
+++ src/components/Chart/ChartBar/index.js
@@ -1,9 +1,19 @@
 import './styles.css';
 
 export function ChartBar(props) {
+  let barFill = '0%';
+
+  if(props.max > 0) barFill = Math.round((props.value / props.maxValue) * 100);
+
   return (
-    <>
-      <h1>ChartBar</h1>
-    </>
+    <div className="chart-bar">
+      <div className="chart-bar__inner">
+        <div 
+        className="chart-bar__fill" 
+        style={{ height: barFill }} 
+        ></div>
+      </div>
+      <div className="chart-bar__label">{props.label}</div>
+    </div>
   );
 };
